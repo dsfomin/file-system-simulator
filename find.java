7d4046dd3b73dcd17de143466635d4b7d1bfeabe
Vladyslav <slava.samokrik@gmail.com>
2021-04-28 23:44:03 +0300

    Work with find

--- find.java
+++ find.java
@@ -1,75 +1,216 @@
-class find {
-    public static String PROGRAM_NAME = "find";
+import java.util.Vector;
 
-    public static void main(String[] args) throws Exception {
-        Kernel.initialize();
+public class find {
 
-        if (args.length != 1) {
-            System.out.println("Wrong \""+ PROGRAM_NAME + "\" command signature!");
-            System.out.println("Usage: " + "java " + PROGRAM_NAME + " <path>");
-            System.out.println("\t<path>" + " :\n\tPath to directory\n\tChecks if it exists\n\tOutputs list of directories and files in directory");
-            Kernel.exit(1);
+    public static String PROGRAM_NAME="find";
+
+    public static void main(Vector<String> args) throws Exception{
+
+        // initialize the file system simulator kernel
+        Kernel.initialize() ;
+
+        // for each path-name given
+        for( int i = 0 ; i < args.size() ; i ++ )
+        {
+            String name = args.elementAt(i) ;
+            int status = 0 ;
+
+            // stat the name to get information about the file or directory
+            Stat stat = new Stat() ;
+            status = Kernel.stat( name , stat ) ;
+            if( status < 0 )
+            {
+                Kernel.perror( PROGRAM_NAME ) ;
+                Kernel.exit( 1 ) ;
+            }
+
+            // mask the file type from the mode
+            short type = (short)( stat.getMode() & Kernel.S_IFMT ) ;
+
+            // if name is a regular file, print the info
+            if( type == Kernel.S_IFREG )
+            {
+                print( name , stat ) ;
+            }
+
+            // if name is a directory open it and read the contents
+            else if( type == Kernel.S_IFDIR )
+            {
+                // open the directory
+                int fd = Kernel.open( name , Kernel.O_RDONLY ) ;
+                if( fd < 0 )
+                {
+                    Kernel.perror( PROGRAM_NAME ) ;
+                    System.err.println( PROGRAM_NAME + ": unable to open \"" + name + "\" for reading" ) ;
+                    Kernel.exit(1) ;
+                }
+
+                // print a heading for this directory
+                System.out.println() ;
+                System.out.println( name + ":" ) ;
+
+                // create a directory entry structure to hold data as we read
+                DirectoryEntry directoryEntry = new DirectoryEntry() ;
+                //int count = 0;
+
+                // while we can read, print the information on each entry
+                while( true )
+                {
+                    // read an entry; quit loop if error or nothing read
+                    status = Kernel.readdir( fd , directoryEntry ) ;
+                    if( status <= 0 )
+                        break ;
+
+                    // get the name from the entry
+                    String entryName = directoryEntry.getName() ;
+
+                    // call stat() to get info about the file
+                    status = Kernel.stat( name + "/" + entryName , stat ) ;
+                    if( status < 0 )
+                    {
+                        Kernel.perror( PROGRAM_NAME ) ;
+                        Kernel.exit( 1 ) ;
+                    }
+
+                    // print the entry information
+                    if (!entryName.endsWith(".")){
+                        if(name.equals("/")){
+                            print(name + entryName,stat);
+                            callFunc(name+entryName,1);
+                        }
+                        else{
+                            print(name+"/"+entryName,stat);
+                            //System.out.println(" check " +name+"/"+entryName);
+                            callFunc(name+"/"+entryName,1);
+                        }
+                    }
+                    //count ++ ;
+                }
+
+                // check to see if our last read failed
+                if( status < 0 )
+                {
+                    Kernel.perror( "main" ) ;
+                    System.err.println( "main: unable to read directory entry from /" ) ;
+                    Kernel.exit(2) ;
+                }
+
+                // close the directory
+                Kernel.close( fd ) ;
+
+                // print a footing for this directory
+                //System.out.println( "total files: " + count ) ;
+            }
         }
 
-        searchFiles(args[0]);
+        // exit with success if we process all the arguments
+        //Kernel.exit( 0 ) ;
     }
 
-    private static void searchFiles(String name) throws Exception {
+    private static void callFunc(String name,int level) throws Exception{
 
-        var stat = new Stat();
-        var status = Kernel.stat(name, stat);
+        int status = 0 ;
 
-        if (status < 0) {
-            Kernel.perror(PROGRAM_NAME);
-            Kernel.exit(1);
+        // stat the name to get information about the file or directory
+        Stat stat = new Stat() ;
+        status = Kernel.stat( name , stat ) ;
+        if( status < 0 )
+        {
+            Kernel.perror( PROGRAM_NAME ) ;
+            Kernel.exit( 1 ) ;
         }
 
-        var type = (short) (stat.getMode() & Kernel.S_IFMT);
+        // mask the file type from the mode
+        short type = (short)( stat.getMode() & Kernel.S_IFMT ) ;
 
-
-        if (type == Kernel.S_IFREG) {
-            System.out.println(name);
+        // if name is a regular file, print the info
+        if( type == Kernel.S_IFREG )
+        {
+            print( name , stat ) ;
         }
 
-        else if (type == Kernel.S_IFDIR) {
-            System.out.println(name);
+        // if name is a directory open it and read the contents
+        else if( type == Kernel.S_IFDIR )
+        {
+            // open the directory
+            int fd = Kernel.open( name , Kernel.O_RDONLY ) ;
+            if( fd < 0 )
+            {
+                Kernel.perror( PROGRAM_NAME ) ;
+                System.err.println( PROGRAM_NAME + ": unable to open \"" + name + "\" for reading" ) ;
+                Kernel.exit(1) ;
+            }
 
-            int fd = Kernel.open(name, Kernel.O_RDONLY);
+            // print a heading for this directory
+            //System.out.println() ;
+            //System.out.println( name + ":" ) ;
 
-            if (fd < 0) {
-                Kernel.perror(PROGRAM_NAME);
-                System.out.println(PROGRAM_NAME + ": unable to open \"" + name + "\" for reading");
-                Kernel.exit(1);
-            } else {
+            // create a directory entry structure to hold data as we read
+            DirectoryEntry directoryEntry = new DirectoryEntry() ;
+            //int count = 0;
 
-                var directoryEntry = new DirectoryEntry();
+            // while we can read, print the information on each entry
+            while( true )
+            {
+                // read an entry; quit loop if error or nothing read
+                status = Kernel.readdir( fd , directoryEntry ) ;
+                if( status <= 0 )
+                    break ;
 
+                // get the name from the entry
+                String entryName = directoryEntry.getName() ;
 
-                while (true) {
+                // call stat() to get info about the file
+                status = Kernel.stat( name + "/" + entryName , stat ) ;
+                if( status < 0 )
+                {
+                    Kernel.perror( PROGRAM_NAME ) ;
+                    Kernel.exit( 1 ) ;
+                }
 
-                    status = Kernel.readdir(fd, directoryEntry);
-                    if (status <= 0)
-                        break;
-
-                    var entryName = directoryEntry.getName();
-
-                    if (!entryName.equals(".") && !entryName.equals("..")) {
-                        var fullName = getFullName(name, entryName);
-                        searchFiles(fullName);
+                // print the entry information
+                if (!entryName.endsWith(".")){
+                    if(name.equals("/")){
+                        for(int i=0;i<=level;i++){System.out.print("  ");}
+                        print(name + entryName,stat);
+                        callFunc(name+entryName,level+1);
+                    }
+                    else{
+                        for(int i=0;i<=level;i++){System.out.print("  ");}
+                        print(name+"/"+entryName,stat);
+                        callFunc(name+"/"+entryName,level+1);
                     }
                 }
+                //count ++ ;
             }
-            Kernel.close(fd);
+
+            // check to see if our last read failed
+            if( status < 0 )
+            {
+                Kernel.perror( "main" ) ;
+                System.err.println( "main: unable to read directory entry from /" ) ;
+                Kernel.exit(2) ;
+            }
+
+            // close the directory
+            Kernel.close( fd ) ;
+
+            // print a footing for this directory
+            //System.out.println( "total files: " + count ) ;
         }
     }
 
-    private static String getFullName(String name, String entryName) {
-        var stringBuilder = new StringBuilder(name);
-        if (!name.equals("/")) {
-            stringBuilder.append("/");
-        }
-        stringBuilder.append(entryName);
+    private static void print( String name , Stat stat )
+    {
+        // a buffer to fill with a line of output
+        StringBuffer s = new StringBuffer() ;
 
-        return stringBuilder.toString();
+        // append the name
+        s.append("   ");
+        s.append( name ) ;
+
+        // print the buffer
+        System.out.println( s.toString() ) ;
     }
+
 }
