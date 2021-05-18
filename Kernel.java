c068c7e9b77756b317fb0531baed521f36ba552b
dsfomin <dsfomin2@gmail.com>
2021-05-18 23:50:31 +0300

    task 5 - chmod

--- Kernel.java
+++ Kernel.java
@@ -468,39 +468,6 @@ public class Kernel
       // return (EACCES) if the file does not exist and the directory
       // in which it is to be created is not writable
 
-
-
-      int protectionInfo = 0;
-
-      if ((mode & S_IFMT) == S_IFDIR) {
-        protectionInfo |= S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH
-                | S_IXOTH;
-      } else if ((mode & S_IFMT) == S_IFREG) {
-        protectionInfo |= S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-      } else {
-        System.out.println("You're trying to create not regular file or directory");
-        return -1;
-      }
-
-      String protectionInfoOctal = Integer.toOctalString(protectionInfo),
-              umaskOctal = String.format("%03o", process.getUmask());
-
-      StringBuilder resultPermission = new StringBuilder();
-
-      for (int i = 0; i < protectionInfoOctal.length(); i++) {
-        if (protectionInfoOctal.charAt(i) >= umaskOctal.charAt(i))
-          resultPermission.append(protectionInfoOctal.charAt(i) - umaskOctal.charAt(i));
-        else
-          resultPermission.append(0);
-      }
-
-      protectionInfo = Integer.parseInt(resultPermission.toString(), 8);
-
-      int oldPermissions = mode % ((int) Math.pow(2, 9)), newMode = mode - oldPermissions + protectionInfo;
-
-
-
-
       currIndexNode.setMode( mode ) ;
       currIndexNode.setNlink( (short)1 ) ;
 
@@ -1223,6 +1190,52 @@ to be done:
        int chdir(const char *path);
        mode_t umask(mode_t mask);
 */
+       public static int chmod( String pathname, short new_mode ) throws Exception {
+        String fullPath = getFullPath( pathname ) ;
+
+        IndexNode indexNode = new IndexNode() ;
+        short indexNodeNumber = findIndexNode( fullPath , indexNode ) ;
+        if (indexNodeNumber < 0 ){
+            Kernel.perror( PROGRAM_NAME ) ;
+            System.err.println( PROGRAM_NAME + ": unable to open file for reading" );
+            Kernel.exit( 1 ) ;
+        }
+
+        if (process.getUid() != 0) {
+            short mode = indexNode.getMode();
+            if (indexNode.getUid() == process.getUid()) {
+                if ( (mode & S_IWUSR) == 0 ) {
+                    Kernel.perror(PROGRAM_NAME);
+                    System.err.println(PROGRAM_NAME + ": permission denied");
+                    Kernel.exit(3);
+                }
+            } else if (indexNode.getGid() == process.getGid()) {
+                if ( (mode & S_IWGRP) == 0 ) {
+                    Kernel.perror(PROGRAM_NAME);
+                    System.err.println(PROGRAM_NAME + ": permission denied");
+                    Kernel.exit(3);
+                }
+            } else {
+                if ( (mode & S_IWOTH) == 0 ) {
+                    Kernel.perror(PROGRAM_NAME);
+                    System.err.println(PROGRAM_NAME + ": permission denied");
+                    Kernel.exit(3);
+                }
+            }
+        }
+
+        if (indexNode.getUid() == process.getUid() || process.getUid() == 0) {
+            indexNode.setMode((short) ((indexNode.getMode() & (~0777)) | new_mode));
+            openFileSystems[ROOT_FILE_SYSTEM].writeIndexNode(indexNode, indexNodeNumber);
+        }
+        else {
+            Kernel.perror( PROGRAM_NAME ) ;
+            System.err.println( PROGRAM_NAME + ": you haven't access" );
+            Kernel.exit( 1 ) ;
+        }
+
+        return 0;
+    }
 
   /**
    * This is an internal variable for the simulator which always 
